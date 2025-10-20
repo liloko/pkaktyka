a39815fd22d21dd642bdfa47aa7a6b1c4db16497
liloko <78354559+liloko@users.noreply.github.com>
2025-10-20 19:13:15 +0300

    практична 1

---
+++ практична 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace speed
+{
+    internal class speed { }
+    public class auto
+    {
+        private class dvygun
+        {
+            public void start()
+            {
+                Console.WriteLine("пєрємога буде");
+            }
+        }
+        public void zapusk()
+        {
+            dvygun d = new dvygun();
+            d.start();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            auto a = new auto();
+            a.zapusk();
+        }
+    }
+}
